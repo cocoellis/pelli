8d192651c355b391583da6d786582f9cd141ddfe
Coco Ellis <mellis1223@gmail.com>
2020-07-14 20:09:53 -0500

    pelli

--- warbler-client/src/containers/App.js
+++ warbler-client/src/containers/App.js
@@ -1,33 +1,13 @@
 import React, { Component } from "react";
-import { Provider } from "react-redux";
-import { configureStore } from "../store";
 import { BrowserRouter as Router } from "react-router-dom";
-import Navbar from "./Navbar";
-import Main from "./Main";
-import { setAuthorizationToken, setCurrentUser } from "../store/actions/auth";
-import jwtDecode from "jwt-decode";
-
-const store = configureStore();
-
-if (localStorage.jwtToken) {
-  setAuthorizationToken(localStorage.jwtToken);
-  // prevent someone from manually tampering with the key of jwtToken in localStorage
-  try {
-    store.dispatch(setCurrentUser(jwtDecode(localStorage.jwtToken)));
-  } catch (e) {
-    store.dispatch(setCurrentUser({}));
-  }
-}
+import Homepage from "../components/Homepage";
 
 const App = () => (
-  <Provider store={store}>
     <Router>
       <div className="onboarding">
-        <Navbar />
-        <Main />
+        <Homepage />
       </div>
     </Router>
-  </Provider>
 );
 
 export default App;
