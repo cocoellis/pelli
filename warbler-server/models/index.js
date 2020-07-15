0769b197b6db0be803e55cefbf1c2d2af1c2eb52
cocoellis <38797064+cocoellis@users.noreply.github.com>
2020-07-14 21:47:00 -0500

    Delete index.js

--- warbler-server/models/index.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-mongoose.set("debug", true);
-mongoose.Promise = Promise;
-mongoose.connect("mongodb://localhost/warbler", {
-  keepAlive: true
-});
-
-module.exports.User = require("./user");
-module.exports.Message = require("./message");
